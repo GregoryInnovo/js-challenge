a64fb6cafaddedf0df60581b122b6725dcc951b1
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-06-07 13:15:16 -0500

    [ADD] solve second problem

--- src/index.js
+++ src/index.js
@@ -2,26 +2,34 @@ const $app = document.getElementById("app");
 const $observe = document.getElementById("observe");
 let offset = 0;
 let limit = 5;
-let API = `https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=${limit}`;
-
+let API;
+let output;
 const getData = (api) => {
   fetch(api)
     .then((response) => response.json())
     .then((response) => {
       let products = response;
-      let output = products.map((product) => {
-        // template
-        console.table(product);
+      output = products.map((product) => {
+        return `
+          <article class="Card">
+            <img src="${product.images[0]}"  alt="${product.description}" width="100%" height="auto" />
+            <h2>
+              ${product.title}
+              <small>$ ${product.price}</small>
+            </h2>
+          </article>
+        `;
       });
       let newItem = document.createElement("section");
-      newItem.classList.add("Item");
-      newItem.innerHTML = output;
+      newItem.classList.add("Items");
+      newItem.innerHTML += output.join(" ");
       $app.appendChild(newItem);
     })
     .catch((error) => console.log(error));
 };
 
 const loadData = () => {
+  API = `https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=${limit}`;
   getData(API);
 };
 
